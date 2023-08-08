d7393019623763528848b29d20f01739c7cae0f7
simondlevy <simon.d.levy@gmail.com>
2023-08-08 19:21:37 -0400

    Cleanup

--- simdvs/__init__.py
+++ simdvs/__init__.py
@@ -54,67 +54,73 @@ class SimDvs:
         # Once we have a current and previous image, we can compute events
         if self.image_prev is not None:
 
-            # Make a first-difference image between the current and previous
-            # grayscale images
-            grayprev = self._color2gray(self.image_prev)
-            diffimg = graycurr - grayprev
-
-            # Simulate positive and negative events w.r.t. a threshold
-            events[diffimg > +self.threshold] = +1
-            events[diffimg < -self.threshold] = -1
-
-            # If sensor resolution was indicated, resize the event image now
-            if self.resolution is not None:
-                events = cv2.resize(events.astype('float32'), (128, 128))
-
-            # If display was requested, set it up
-            if self.display_scale > 0:
-
-                # Make a color image from the event image
-                rows, cols = events.shape
-                ceventimg = np.zeros((rows, cols, 3))
-
-                # If color was indicated, display positive events as green,
-                # negative as red
-                if self.colorize:
-                    ceventimg[events == +1, 1] = 255
-                    ceventimg[events == -1, 2] = 255
-
-                # Otherwise, display all events as white
-                else:
-                    ceventimg[events != 0, :] = 255
-
-                # Support annotating the event image in a subclass
-                self.annotate(ceventimg)
-
-                # Make two-column image to display the original and events
-                rows, cols = events.shape
-                bigimg = np.zeros((rows, 2*cols, 3)).astype(np.uint8)
-
-                # Fill the first column with the original (resized if
-                # indicated)
-                bigimg[:, :cols, :] = (
-                        image if self.resolution is None
-                        else cv2.resize(image, self.resolution))
-
-                # fill the second column with the events image
-                bigimg[:, cols:(2*cols), :] = ceventimg
-
-                # Display the two-colum image
-                cv2.imshow('Events',
-                           cv2.resize(bigimg,
-                                      (self.display_scale *
-                                       bigimg.shape[1],
-                                       self.display_scale *
-                                       bigimg.shape[0])))
-
-                # If the user hit the quit key, set the events image to None,
-                # indicating quit
-                if cv2.waitKey(1) == self.quit_key:
-                    events = None
+            events = self._update(image, graycurr, events)
 
         # Track the previous image for first-differencing
         self.image_prev = image
+
+        return events
+
+    def _update(self, image, graycurr, events):
+
+        # Make a first-difference image between the current and previous
+        # grayscale images
+        grayprev = self._color2gray(self.image_prev)
+        diffimg = graycurr - grayprev
+
+        # Simulate positive and negative events w.r.t. a threshold
+        events[diffimg > +self.threshold] = +1
+        events[diffimg < -self.threshold] = -1
+
+        # If sensor resolution was indicated, resize the event image now
+        if self.resolution is not None:
+            events = cv2.resize(events.astype('float32'), (128, 128))
+
+        # If display was requested, set it up
+        if self.display_scale > 0:
+
+            # Make a color image from the event image
+            rows, cols = events.shape
+            ceventimg = np.zeros((rows, cols, 3))
+
+            # If color was indicated, display positive events as green,
+            # negative as red
+            if self.colorize:
+                ceventimg[events == +1, 1] = 255
+                ceventimg[events == -1, 2] = 255
+
+            # Otherwise, display all events as white
+            else:
+                ceventimg[events != 0, :] = 255
+
+            # Support annotating the event image in a subclass
+            self.annotate(ceventimg)
+
+            # Make two-column image to display the original and events
+            rows, cols = events.shape
+            bigimg = np.zeros((rows, 2*cols, 3)).astype(np.uint8)
+
+            # Fill the first column with the original (resized if
+            # indicated)
+            bigimg[:, :cols, :] = (
+                    image if self.resolution is None
+                    else cv2.resize(image, self.resolution))
+
+            # fill the second column with the events image
+            bigimg[:, cols:(2*cols), :] = ceventimg
+
+            # Display the two-colum image
+            cv2.imshow('Events',
+                       cv2.resize(bigimg,
+                                  (self.display_scale *
+                                   bigimg.shape[1],
+                                   self.display_scale *
+                                   bigimg.shape[0])))
+
+            # If the user hit the quit key, set the events image to None,
+            # indicating quit
+            if cv2.waitKey(1) == self.quit_key:
+                return None
 
         return events
 
